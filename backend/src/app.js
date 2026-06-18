e85e295dbb3159ccac142e8175f39fc79a7bf599
vudz2310 <vudz2310@gmail.com>
2026-06-18 19:11:07 +0700

    feat: Thêm swagger cho backend tích hợp login và register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,21 @@
-import express from 'express';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import authRouter from "./routes/auth.route.js";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerSpec from "./docs/swagger.js";
 
 const app = express();
 
 // Custom CORS Middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
@@ -17,10 +23,11 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Server is running' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK", message: "Server is running" });
 });
 
 export default app;
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,8 +3,84 @@ import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary:
+ *       Đăng nhập
+ *
+ *     tags:
+ *       - Auth
+ *
+ *     requestBody:
+ *       required: true
+ *
+ *       content:
+ *         application/json:
+ *
+ *           schema:
+ *
+ *             type: object
+ *
+ *             properties:
+ *
+ *               email:
+ *                 type: string
+ *
+ *               password:
+ *                 type: string
+ *
+ *     responses:
+ *
+ *       200:
+ *
+ *         description:
+ *           Thành công
+ */
 
 router.post("/login", login);
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary:
+ *       Đăng kí
+ *
+ *     tags:
+ *       - Auth
+ *
+ *     requestBody:
+ *       required: true
+ *
+ *       content:
+ *         application/json:
+ *
+ *           schema:
+ *
+ *             type: object
+ *
+ *             properties:
+ *
+ *               email:
+ *                 type: string
+ *
+ *               password:
+ *                 type: string
+ *
+ *               full_name:
+ *                 type: string
+ *
+ *               phone:
+ *                 type: string
+ *
+ *     responses:
+ *
+ *       200:
+ *
+ *         description:
+ *           Thành công
+ */
 router.post("/register", register);
 
 export default router;
