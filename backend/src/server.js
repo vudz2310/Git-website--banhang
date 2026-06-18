3f5e084daee9d716e0af36448539b2400e6a58f6
vudz2310 <vudz2310@gmail.com>
2026-06-18 19:11:07 +0700

    feat: Thêm swagger cho backend tích hợp login và register

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
-import app from './app.js';
-import { port } from './config/env.js';
-import { connectDB } from './config/database.js';
+import app from "./app.js";
 
-const PORT = port;
+import { port } from "./config/env.js";
+
+import { connectDB } from "./config/database.js";
+
+const PORT = port || 5000;
 
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`URL: http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
+
+  console.log(
+    `Swagger:
+http://localhost:${PORT}/swagger`,
+  );
 });
